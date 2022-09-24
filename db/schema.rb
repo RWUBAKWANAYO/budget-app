baa19d1156275bec8ea130258b675132b5098386
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-09-24 08:53:22 +0200

    setup devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_24_064516) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_24_065157) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,8 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_24_064516) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_groups_on_user_id"
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_groups_on_author_id"
   end
 
   create_table "user_transactions", force: :cascade do |t|
@@ -28,19 +28,26 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_24_064516) do
     t.integer "amount", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.bigint "groups_id", null: false
-    t.index ["groups_id"], name: "index_user_transactions_on_groups_id"
-    t.index ["user_id"], name: "index_user_transactions_on_user_id"
+    t.bigint "author_id", null: false
+    t.bigint "group_id", null: false
+    t.index ["author_id"], name: "index_user_transactions_on_author_id"
+    t.index ["group_id"], name: "index_user_transactions_on_group_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "groups", "users"
-  add_foreign_key "user_transactions", "groups", column: "groups_id"
-  add_foreign_key "user_transactions", "users"
+  add_foreign_key "groups", "users", column: "author_id"
+  add_foreign_key "user_transactions", "groups"
+  add_foreign_key "user_transactions", "users", column: "author_id"
 end
