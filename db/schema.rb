a653e72e6a10205ca27f88a699f464c0a6390d4d
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-09-24 08:46:35 +0200

    add tables references

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_24_064107) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_24_064516) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_24_064107) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "user_transactions", force: :cascade do |t|
@@ -26,6 +28,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_24_064107) do
     t.integer "amount", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "groups_id", null: false
+    t.index ["groups_id"], name: "index_user_transactions_on_groups_id"
+    t.index ["user_id"], name: "index_user_transactions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_24_064107) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "groups", "users"
+  add_foreign_key "user_transactions", "groups", column: "groups_id"
+  add_foreign_key "user_transactions", "users"
 end
