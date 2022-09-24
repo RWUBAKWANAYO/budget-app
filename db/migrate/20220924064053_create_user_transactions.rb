48d7338830a0ece40c57fc8d930a779496a0bac9
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2022-09-24 08:44:31 +0200

    create database table

--- db/migrate/20220924064053_create_user_transactions.rb
+++ db/migrate/20220924064053_create_user_transactions.rb
@@ -2,7 +2,7 @@ class CreateUserTransactions < ActiveRecord::Migration[7.0]
   def change
     create_table :user_transactions do |t|
       t.string :name
-      t.integer :amount
+      t.integer :amount, :default => 0
 
       t.timestamps
     end
